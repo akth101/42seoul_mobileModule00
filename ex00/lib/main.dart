103f9e4123d7137caeea5c73c81650e79f7e642a
Seongjko <rhtjdwns2345@gmail.com>
2025-01-10 20:21:46 +0900

    completed ex00

--- ex00/lib/main.dart
+++ ex00/lib/main.dart
@@ -4,37 +4,63 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.sizeOf(context);
+
     return MaterialApp(
       home: Scaffold(
-        body: basicDisplay(),
+        body: basicDisplay(screenSize.width, screenSize.height),
       ),
-      );
+    );
   }
 
-  Center basicDisplay() {
+  Center basicDisplay(double screenWidth, double screenHeight) {
     return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 100,
-              height: 30,
-              color: Colors.amber,
-              child: const Center(child: Text("A Simple text")),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: screenWidth * 0.2,
+            height: screenHeight * 0.08,
+            color: Colors.amber,
+            child: Center(
+              child: Text(
+                "A Simple text",
+                style: TextStyle(
+                  fontSize: screenWidth * 0.03,
+                ),
+              ),
             ),
-            const SizedBox(
-              height: 10,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: screenWidth * 0.2,
+            height: screenHeight * 0.1,
+            child: TextButton(
+              onPressed: () {
+                debugPrint("Button pressed");
+              },
+              child: Text(
+                "Click me",
+                style: TextStyle(
+                  fontSize: screenWidth * 0.03,
+                ),
+              ),
             ),
-            TextButton(onPressed: () {
-              debugPrint("Button pressed");
-            }, child: const Text("Click me")),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
   }
 }
