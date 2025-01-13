71dfbcd6ad5ba0bf8cf70020b6200b2c7a6101b2
Seongjko <rhtjdwns2345@gmail.com>
2025-01-13 15:08:11 +0900

    completed ex02

--- ex02/lib/Calculator.dart
+++ ex02/lib/Calculator.dart
@@ -12,6 +12,7 @@ class Calculator extends StatefulWidget {
 class _CalculatorState extends State<Calculator> {
   String expression = "0";
   String result = "0";
+
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.sizeOf(context);
@@ -35,65 +36,98 @@ class _CalculatorState extends State<Calculator> {
 
   Expanded buttonField(Size screenSize) {
     return Expanded(
-            child: Column(
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
               children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Row(
-                    children: [
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                      Expanded(child: indivButton("5", screenSize)),
-                    ],
-                  ),
-                ),
+                Expanded(child: indivButton("7", screenSize)),
+                Expanded(child: indivButton("8", screenSize)),
+                Expanded(child: indivButton("9", screenSize)),
+                Expanded(child: specialButton("C", screenSize)),
+                Expanded(child: specialButton("AC", screenSize)),
               ],
             ),
-          );
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: indivButton("4", screenSize)),
+                Expanded(child: indivButton("5", screenSize)),
+                Expanded(child: indivButton("6", screenSize)),
+                Expanded(child: indivButton("+", screenSize)),
+                Expanded(child: indivButton("-", screenSize)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: indivButton("1", screenSize)),
+                Expanded(child: indivButton("2", screenSize)),
+                Expanded(child: indivButton("3", screenSize)),
+                Expanded(child: indivButton("*", screenSize)),
+                Expanded(child: indivButton("/", screenSize)),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(child: indivButton("0", screenSize)),
+                Expanded(child: indivButton(".", screenSize)),
+                Expanded(child: indivButton("00", screenSize)),
+                Expanded(child: indivButton("=", screenSize)),
+                Expanded(child: indivButton("", screenSize)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   GestureDetector indivButton(String text, Size screenSize) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        if (text != "") {
+          debugPrint("button pressed: $text");
+        }
+      },
       child: Container(
         width: double.infinity,
         height: double.infinity,
         color: Colors.teal[100],
-        child: Text(text),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(fontSize: screenSize.height * 0.045),
+          ),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector specialButton(String text, Size screenSize) {
+    return GestureDetector(
+      onTap: () {
+        if (text != "") {
+          debugPrint("button pressed: $text");
+        }
+      },
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.teal[100],
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontSize: screenSize.height * 0.045,
+                color: Colors.red
+                ),
+          ),
+        ),
       ),
     );
   }
